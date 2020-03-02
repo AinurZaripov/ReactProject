9d2f796b2be0bc277c3dd81b12ecfde04f8a60cc
Айнур <zaripow.aynur@yandex.ru>
2020-03-02 13:13:15 +0300

    Add project files.

---
+++ ReactProject/Controllers/PhonesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactProject.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactProject.Controllers
+{
+    [Route("api/[controller]")]
+    public class PhonesController : Controller
+    {
+        static readonly List<Phone> data;
+        static PhonesController()
+        {
+            data = new List<Phone>
+            {
+                new Phone { Id = Guid.NewGuid().ToString(), Name = "Xiaomi mi 9t", Price = 20000},
+                new Phone { Id = Guid.NewGuid().ToString(), Name = "Iphone 11", Price = 70000}
+            };
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Phone> Get()
+        {
+            return data;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post(Phone phone)
+        {
+            phone.Id = Guid.NewGuid().ToString();
+            data.Add(phone);
+            return Ok(phone);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            Phone phone = data.FirstOrDefault(f => f.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+            data.Remove(phone);
+            return Ok(phone);
+        }
+    }
+}
